e8953bf1bc9c38f7cc9c601fc523d2f439d6d7c5
Rafael Santos <rafa.05125@gmail.com>
2023-03-15 13:44:26 -0300

    Project hr-worker created

---
+++ hr-workder/src/main/java/com/rafaelsantos/hrworker/HrWorkderApplication.java
@@ -0,0 +1,13 @@
+package com.rafaelsantos.hrworker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrWorkderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrWorkderApplication.class, args);
+	}
+
+}
