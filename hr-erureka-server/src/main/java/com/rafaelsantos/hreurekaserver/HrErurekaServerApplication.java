89a684535a157f6edda157221b027c22f56c6e3e
Rafael Santos <rafa.05125@gmail.com>
2023-03-18 13:45:06 -0300

    Project hr-eureka-server-created

---
+++ hr-erureka-server/src/main/java/com/rafaelsantos/hreurekaserver/HrErurekaServerApplication.java
@@ -0,0 +1,13 @@
+package com.rafaelsantos.hreurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrErurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrErurekaServerApplication.class, args);
+	}
+
+}
